2ce2a9aa51d382343e0b9616818108549d9472ed
Developer-Phil-Youtube <76846873+Developer-Phil-Youtube@users.noreply.github.com>
2021-01-01 13:22:41 -0500

    Update variables.py

--- Basic Fundamentals/variables.py
+++ Basic Fundamentals/variables.py
@@ -1,1 +1,11 @@
+#These are variables in which you can store certain things.
+variable_one = "Hello My name is Phil"
+variable_two = ["red", "blue", "green", "yellow"]
+variable_3 = 14
+variable_4 = 14.5
 
+#you can print these variable or in other words, you can call the variable and print it.
+print(variable_one)
+print(variable_two)
+print(variable_3)
+print(variable_4)

acb8b2a347f5887b39036d7fca9e4b73f0b2acde
Developer-Phil-Youtube <76846873+Developer-Phil-Youtube@users.noreply.github.com>
2021-01-01 13:27:19 -0500

    Update variables.py

--- Basic Fundamentals/variables.py
+++ Basic Fundamentals/variables.py
@@ -4,6 +4,8 @@ variable_two = ["red", "blue", "green", "yellow"]
 variable_3 = 14
 variable_4 = 14.5
 
+#variable_5 = input("What is your name? ")
+
 #you can print these variable or in other words, you can call the variable and print it.
 print(variable_one)
 print(variable_two)
